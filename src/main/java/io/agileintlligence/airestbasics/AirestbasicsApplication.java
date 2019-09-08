af3249b37b10cdf312c00fc713a5fba12d487534
AgileIntelligence <io.agileintelligence@gmail.com>
2019-09-08 09:28:53 -0400

    first commit

---
+++ src/main/java/io/agileintlligence/airestbasics/AirestbasicsApplication.java
@@ -0,0 +1,13 @@
+package io.agileintlligence.airestbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirestbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirestbasicsApplication.class, args);
+	}
+
+}
